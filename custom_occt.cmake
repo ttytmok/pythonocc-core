605aaebedc3f4d0f8c03ba5df2bc2879f058fa08
yourname <your@email.com>
2025-05-04 15:09:23 +0900

    記録する

---
+++ custom_occt.cmake
@@ -0,0 +1,4 @@
+set(OpenCASCADE_INCLUDE_DIR "${OCCT_ROOT}/occt-vc144-64/inc")
+set(OpenCASCADE_LIBRARY_DIR "${OCCT_ROOT}/occt-vc144-64/lib")
+set(THIRD_PARTY_INCLUDE_DIR "${OCCT_ROOT}/3rdparty-vc14-64/include")
+set(THIRD_PARTY_LIBRARY_DIR "${OCCT_ROOT}/3rdparty-vc14-64/lib")
